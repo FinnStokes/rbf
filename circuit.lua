0870894a60d93aef4b481aa142bab497f1751c85
Finn Stokes <finn.stokes@gmail.com>
2012-08-25 22:32:03 +0930

    Selecting regions working. Need to implement pasting.

--- circuit.lua
+++ circuit.lua
@@ -1,15 +1,17 @@
 -- circuit.lua
 local M = {}
 
-M.new = function (arg)
+function M.new(arg)
    arg = arg or {}
    local self = {
       width = arg.width or 16,
       height = arg.height or 16,
-      cmap = arg.colourmap or {{16,16,16}, {0,0,255}, {255,0,0}, {255,255,0}},
    }
    self.scale = arg.scale or (arg.size or 512) / self.width
-   local object = {}
+   local object = {
+      colourmap = arg.colourmap or {{16,16,16}, {0,0,255}, {255,0,0}, {255,255,0}},
+      linecolour = arg.linecolour or {0,0,0},
+   }
    if arg.cells then
       if #arg.cells ~= self.width*self.height then
          error("Invalid circuit layout")
@@ -24,15 +26,15 @@ M.new = function (arg)
       end
    end
    
-   object.draw = function()
+   function object.draw()
       for y = 1,self.height do
          for x = 1,self.width do
-            local colour = self.cmap[object.cells[x+(y-1)*self.width]+1] or {0,0,0}
+            local colour = object.colourmap[object.cells[x+(y-1)*self.width]+1] or {0,0,0}
             love.graphics.setColor(colour)
             love.graphics.rectangle("fill",(x-1)*self.scale, (y-1)*self.scale, self.scale, self.scale)
          end
       end
-      love.graphics.setColor({0,0,0})
+      love.graphics.setColor(object.linecolour)
       love.graphics.setLine(1, "smooth")
       for y = 0,self.height do
          love.graphics.line(0, y*self.scale, self.width*self.scale, y*self.scale)
@@ -43,7 +45,7 @@ M.new = function (arg)
       love.graphics.setColor({255,255,255})
    end
    
-   local isActive = function (x,y) 
+   local function isActive(x,y) 
       if x <= 0 then return false end
       if y <= 0 then return false end
       if x > self.width then return false end
@@ -51,7 +53,7 @@ M.new = function (arg)
       return object.cells[x+(y-1)*self.width] == 1
   end
    
-   object.update = function()
+   function object.update()
       local new = {}
       for y = 1,self.height do
          for x = 1,self.width do
@@ -90,7 +92,7 @@ M.new = function (arg)
       object.cells = new
    end
    
-   object.toggleoff = function (x,y)
+   function object.toggleoff(x,y)
       if x < 0 then return -1 end
       if y < 0 then return -1 end
       if x > self.width*self.scale then return -1 end
@@ -104,7 +106,7 @@ M.new = function (arg)
       return object.cells[i]
    end
 
-   object.cyclewire = function (x,y)
+   function object.cyclewire(x,y)
       if x < 0 then return -1 end
       if y < 0 then return -1 end
       if x > self.width*self.scale then return -1 end
@@ -116,7 +118,7 @@ M.new = function (arg)
       return object.cells[i]
    end
    
-   object.setcell = function (x,y,val)
+   function object.setcell(x,y,val)
       if x < 0 then return end
       if y < 0 then return end
       if x > self.width*self.scale then return end
@@ -124,6 +126,37 @@ M.new = function (arg)
       local i =  math.ceil(x / self.scale) + math.ceil(y / self.scale - 1)*self.width
       object.cells[i] = val
    end
+
+   function object.getsubcircuit(x1, y1, x2, y2)
+      if x1 < 0  or y1 < 0 or x1 > self.width*self.scale or y1 > self.width*self.scale or
+        x2 < 0  or y2 < 0 or x2 > self.width*self.scale or y2 > self.width*self.scale then
+        return M.new({width = 1, height = 1, scale = self.scale})
+      end
+      local xmin = math.floor(math.min(x1,x2)/self.scale)
+      local ymin = math.floor(math.min(y1,y2)/self.scale)
+      local width = math.ceil(math.max(x1,x2)/self.scale)-xmin
+      local height = math.ceil(math.max(y1,y2)/self.scale)-ymin
+      local ret = {width = width, height = height, cells = {}, scale = self.scale}
+      for y = 1,ret.height do
+         for x = 1,ret.width do
+            ret.cells[x + (y-1)*ret.width] = object.cells[xmin + x + (ymin + y - 1)*self.width]
+         end
+      end
+      return M.new(ret)
+   end
+
+   function object.width()
+      return self.width
+   end
+
+   function object.height()
+      return self.height
+   end
+
+   function object.scale()
+      return self.scale
+   end
+
    return object
 end
 
--- main.lua
+++ main.lua
@@ -2,17 +2,37 @@ local circuit = require "circuit"
 
 local c = {}
 
+local paused = false
+local selecting = false
+local selected = nil
+local time = 0
+local dragAction = -1
+local startX = -1
+local startY = -1
+
 function love.load()
    c = circuit.new({size=512,width=32,height=32})
 end
 
 function love.draw()
    c.draw()
+   if selecting then
+      love.graphics.setLine(1, "smooth")
+      love.graphics.setColor({200,200,200})
+      local scale = c.scale()
+      local x = math.floor(math.min(love.mouse.getX(),startX)/scale)
+      local y = math.floor(math.min(love.mouse.getY(),startY)/scale)
+      local w = math.ceil(math.max(love.mouse.getX(),startX)/scale)-x
+      local h = math.ceil(math.max(love.mouse.getY(),startY)/scale)-y
+      love.graphics.rectangle("fill", x*scale, y*scale, w*scale, h*scale)
+   elseif selected and (selected.width() > 1 or selected.height() > 1) then
+      local scale = selected.scale()
+      local x = math.floor(love.mouse.getX()/scale)
+      local y = math.floor(love.mouse.getY()/scale)
+      love.graphics.translate(x*scale, y*scale)
+      selected.draw()
+   end
 end
-
-local paused = false
-local time = 0
-local dragAction = -1
 
 function love.update(dt)
    if not paused then
@@ -24,25 +44,34 @@ function love.update(dt)
    end
    
    if dragAction >= 0 then
-      c.setcell(love.mouse.getX(), love.mouse.getY(), dragAction)
+      local x = love.mouse.getX()
+      local y = love.mouse.getY()
+      c.setcell(x, y, dragAction)
    end
 end
+
 function love.mousepressed(x, y, button)
    if button == 'l' then
       dragAction = c.toggleoff(x,y)
    elseif button == 'r' then
-      c.cyclewire(x,y)
+      selecting = true
+      startX = x
+      startY = y
    end
 end
 
 function love.mousereleased(x, y, button)
    if button == 'l' then
       dragAction = -1
+   elseif button == 'r' then
+      selecting = false
+      selected = c.getsubcircuit(startX,startY,x,y)
+      selected.colourmap = {{16,16,16,100}, {0,0,255,100}, {255,0,0,100}, {255,255,0,100}}
+      selected.linecolour = {200,200,200}
    end
 end
 
 function love.keypressed(key, unicode)
-   print(key)
    if key == 'escape' then
       (love.event.quit or love.event.push)('q')
    elseif key == ' ' then
